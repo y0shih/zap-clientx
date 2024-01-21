b4067db5c44b829cca19a37d33851b5bf9d05b42
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-01-21 21:56:47 +0000

    Update Weapons.hpp

--- Utils/Weapons.hpp
+++ Utils/Weapons.hpp
@@ -135,3 +135,47 @@ namespace RCSId {
     int WEAPON_PROWLER = 102;
     int WEAPON_KRABER = 92;
 };
+
+namespace SkinChangerID {
+    int WEAPON_HANDS = 0;
+    //Light
+    int WEAPON_P2020 = 105;
+    int WEAPON_RE45 = 81;
+    int WEAPON_ALTERNATOR = 80;
+    int WEAPON_R99 = 104;
+    int WEAPON_R301 = 0;
+    int WEAPON_SPITFIRE = 106;
+    int WEAPON_G7 = 89;
+    
+    //Heavy
+    int WEAPON_CAR = 112;
+    int WEAPON_RAMPAGE = 21;
+    int WEAPON_REPEATER = 111;
+    int WEAPON_HEMLOCK = 90;
+    int WEAPON_FLATLINE = 88;
+    
+    //Energy
+    int WEAPON_NEMESIS = 113;
+    int WEAPON_VOLT = 110;
+    int WEAPON_TRIPLETAKE = 107;
+    int WEAPON_LSTAR = 93;
+    int WEAPON_DEVOTION = 84;
+    int WEAPON_HAVOC = 86;
+    
+    //Shotgun
+    int WEAPON_MOZAMBIQUE = 96;
+    int WEAPON_EVA8 = 87;
+    int WEAPON_PEACEKEEPER = 103;
+    int WEAPON_MASTIFF = 95;
+    
+    //Snipers
+    int WEAPON_SENTINEL = 1;
+    int WEAPON_CHARGE_RIFLE = 83;
+    int WEAPON_LONGBOW = 85; 
+    
+    //Legendary
+    int WEAPON_WINGMAN = 109; //Emotional damage!
+    int WEAPON_PROWLER = 102;
+    int WEAPON_BOCEK = 2;
+    int WEAPON_KRABER = 92;
+};
