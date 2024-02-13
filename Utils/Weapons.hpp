4499801610f20530cf7cf3868e65d8af9e21faf0
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-02-13 21:55:41 +0000

    Update Weapons.hpp

--- Utils/Weapons.hpp
+++ Utils/Weapons.hpp
@@ -6,177 +6,191 @@
 namespace AimbotId {
     //Weapon Ids
     //Light
-    int WEAPON_P2020 = 105;
+    int WEAPON_P2020 = 106;
     int WEAPON_RE45 = 81;
     int WEAPON_ALTERNATOR = 80;
-    int WEAPON_R99 = 104;
+    int WEAPON_R99 = 105;
     int WEAPON_R301 = 0;
-    int WEAPON_SPITFIRE = 106;
-    int WEAPON_G7 = 89;
-    
+    int WEAPON_SPITFIRE = 107;
+    int WEAPON_G7 = 90;
+
     //Heavy
-    int WEAPON_CAR = 112;
-    int WEAPON_RAMPAGE = 21;
-    int WEAPON_REPEATER = 111;
-    int WEAPON_HEMLOCK = 90;
-    int WEAPON_FLATLINE = 88;
-    
-    //Energy
-    int WEAPON_NEMESIS = 113;
-    int WEAPON_VOLT = 110;
-    int WEAPON_TRIPLETAKE = 107;
-    int WEAPON_LSTAR = 93;
-    int WEAPON_DEVOTION = 84;
-    int WEAPON_HAVOC = 86;
-    
-    //Shotgun
-    int WEAPON_MOZAMBIQUE = 96;
-    int WEAPON_EVA8 = 87;
-    int WEAPON_PEACEKEEPER = 103;
-    int WEAPON_MASTIFF = 95;
-    
-    //Snipers
-    int WEAPON_SENTINEL = 1;
-    int WEAPON_CHARGE_RIFLE = 83;
-    int WEAPON_LONGBOW = 85; 
-    
-    //Legendary
-    int WEAPON_WINGMAN = 109; //Emotional damage!
+    int WEAPON_FLATLINE = 89;
+    int WEAPON_HEMLOCK = 91;
     int WEAPON_PROWLER = 102;
+    int WEAPON_REPEATER = 112;
+    int WEAPON_RAMPAGE = 21;
+    int WEAPON_CAR = 113;
+
+    //Energy
+    int WEAPON_HAVOC = 86;
+    int WEAPON_DEVOTION = 84;
+    int WEAPON_LSTAR = 94;
+    int WEAPON_TRIPLETAKE = 108;
+    int WEAPON_VOLT = 111;
+    int WEAPON_NEMESIS = 114;
+
+    //Shotgun
+    int WEAPON_MOZAMBIQUE = 97;
+    int WEAPON_PEACEKEEPER = 104;
+    int WEAPON_MASTIFF = 96;
+
+    //Sniper
+    int WEAPON_LONGBOW = 85;
+    int WEAPON_CHARGE_RIFLE = 83;
+    int WEAPON_SENTINEL = 1;
+
+    //Legendary
+    int WEAPON_WINGMAN = 110;
+    int WEAPON_EVA8 = 88;
     int WEAPON_BOCEK = 2;
-    int WEAPON_KRABER = 92;
-    int WEAPON_KNIFE = 163;
+    int WEAPON_KRABER = 93;
+    int WEAPON_KNIFE = 166;
+
+    //Melee
+    int WEAPON_HANDS = 115;
 };
 
 namespace TriggerbotId {
     //Weapon Ids
     //Light
-    int WEAPON_P2020 = 105;
+    int WEAPON_P2020 = 106;
     int WEAPON_RE45 = 81;
     int WEAPON_ALTERNATOR = 80;
-    int WEAPON_R99 = 104;
+    int WEAPON_R99 = 105;
     int WEAPON_R301 = 0;
-    int WEAPON_SPITFIRE = 106;
-    int WEAPON_G7 = 89;
-    
+    int WEAPON_SPITFIRE = 107;
+    int WEAPON_G7 = 90;
+
     //Heavy
-    int WEAPON_CAR = 112;
-    int WEAPON_RAMPAGE = 21;
-    int WEAPON_REPEATER = 111;
-    int WEAPON_HEMLOCK = 90;
-    int WEAPON_FLATLINE = 88;
-    
-    //Energy
-    int WEAPON_NEMESIS = 113;
-    int WEAPON_VOLT = 110;
-    int WEAPON_TRIPLETAKE = 107;
-    int WEAPON_LSTAR = 93;
-    int WEAPON_DEVOTION = 84;
-    int WEAPON_HAVOC = 86;
-    
-    //Shotgun
-    int WEAPON_MOZAMBIQUE = 96;
-    int WEAPON_EVA8 = 87;
-    int WEAPON_PEACEKEEPER = 103;
-    int WEAPON_MASTIFF = 95;
-    
-    //Snipers
-    int WEAPON_SENTINEL = 1;
-    int WEAPON_CHARGE_RIFLE = 83;
-    int WEAPON_LONGBOW = 85; 
-    
-    //Legendary
-    int WEAPON_WINGMAN = 109; //Emotional damage!
+    int WEAPON_FLATLINE = 89;
+    int WEAPON_HEMLOCK = 91;
     int WEAPON_PROWLER = 102;
+    int WEAPON_REPEATER = 112;
+    int WEAPON_RAMPAGE = 21;
+    int WEAPON_CAR = 113;
+
+    //Energy
+    int WEAPON_HAVOC = 86;
+    int WEAPON_DEVOTION = 84;
+    int WEAPON_LSTAR = 94;
+    int WEAPON_TRIPLETAKE = 108;
+    int WEAPON_VOLT = 111;
+    int WEAPON_NEMESIS = 114;
+
+    //Shotgun
+    int WEAPON_MOZAMBIQUE = 97;
+    int WEAPON_PEACEKEEPER = 104;
+    int WEAPON_MASTIFF = 96;
+
+    //Sniper
+    int WEAPON_LONGBOW = 85;
+    int WEAPON_CHARGE_RIFLE = 83;
+    int WEAPON_SENTINEL = 1;
+
+    //Legendary
+    int WEAPON_WINGMAN = 110;
+    int WEAPON_EVA8 = 88;
     int WEAPON_BOCEK = 2;
-    int WEAPON_KRABER = 92;
-    int WEAPON_KNIFE = 163;
+    int WEAPON_KRABER = 93;
+    int WEAPON_KNIFE = 166;
+
+    //Melee
+    int WEAPON_HANDS = 115;
 };
 
 namespace RCSId {
     //Weapon Ids
     //Light
-    int WEAPON_P2020 = 105;
+    int WEAPON_P2020 = 106;
     int WEAPON_RE45 = 81;
     int WEAPON_ALTERNATOR = 80;
-    int WEAPON_R99 = 104;
+    int WEAPON_R99 = 105;
     int WEAPON_R301 = 0;
-    int WEAPON_SPITFIRE = 106;
-    int WEAPON_G7 = 89;
-    
+    int WEAPON_SPITFIRE = 107;
+    int WEAPON_G7 = 90;
+
     //Heavy
-    int WEAPON_CAR = 112;
-    int WEAPON_RAMPAGE = 21;
-    int WEAPON_REPEATER = 111;
-    int WEAPON_HEMLOCK = 90;
-    int WEAPON_FLATLINE = 88;
-    
-    //Energy
-    int WEAPON_NEMESIS = 113;
-    int WEAPON_VOLT = 110;
-    int WEAPON_TRIPLETAKE = 107;
-    int WEAPON_LSTAR = 93;
-    int WEAPON_DEVOTION = 84;
-    int WEAPON_HAVOC = 86;
-    
-    //Shotgun
-    int WEAPON_MOZAMBIQUE = 96;
-    int WEAPON_EVA8 = 87;
-    int WEAPON_PEACEKEEPER = 103;
-    int WEAPON_MASTIFF = 95;
-    
-    //Snipers
-    int WEAPON_SENTINEL = 1;
-    int WEAPON_CHARGE_RIFLE = 83;
-    int WEAPON_LONGBOW = 85; 
-    
-    //Legendary
-    int WEAPON_WINGMAN = 109; //Emotional damage!
+    int WEAPON_FLATLINE = 89;
+    int WEAPON_HEMLOCK = 91;
     int WEAPON_PROWLER = 102;
-    int WEAPON_KRABER = 92;
+    int WEAPON_REPEATER = 112;
+    int WEAPON_RAMPAGE = 21;
+    int WEAPON_CAR = 113;
+
+    //Energy
+    int WEAPON_HAVOC = 86;
+    int WEAPON_DEVOTION = 84;
+    int WEAPON_LSTAR = 94;
+    int WEAPON_TRIPLETAKE = 108;
+    int WEAPON_VOLT = 111;
+    int WEAPON_NEMESIS = 114;
+
+    //Shotgun
+    int WEAPON_MOZAMBIQUE = 97;
+    int WEAPON_PEACEKEEPER = 104;
+    int WEAPON_MASTIFF = 96;
+
+    //Sniper
+    int WEAPON_LONGBOW = 85;
+    int WEAPON_CHARGE_RIFLE = 83;
+    int WEAPON_SENTINEL = 1;
+
+    //Legendary
+    int WEAPON_WINGMAN = 110;
+    int WEAPON_EVA8 = 88;
+    int WEAPON_BOCEK = 2;
+    int WEAPON_KRABER = 93;
+    int WEAPON_KNIFE = 166;
+
+    //Melee
+    int WEAPON_HANDS = 115;
 };
 
 namespace SkinChangerID {
+    //Weapon Ids
     //Light
-    int WEAPON_P2020 = 105;
+    int WEAPON_P2020 = 106;
     int WEAPON_RE45 = 81;
     int WEAPON_ALTERNATOR = 80;
-    int WEAPON_R99 = 104;
+    int WEAPON_R99 = 105;
     int WEAPON_R301 = 0;
-    int WEAPON_SPITFIRE = 106;
-    int WEAPON_G7 = 89;
-    
+    int WEAPON_SPITFIRE = 107;
+    int WEAPON_G7 = 90;
+
     //Heavy
-    int WEAPON_CAR = 112;
-    int WEAPON_RAMPAGE = 21;
-    int WEAPON_REPEATER = 111;
-    int WEAPON_HEMLOCK = 90;
-    int WEAPON_FLATLINE = 88;
-    
-    //Energy
-    int WEAPON_NEMESIS = 113;
-    int WEAPON_VOLT = 110;
-    int WEAPON_TRIPLETAKE = 107;
-    int WEAPON_LSTAR = 93;
-    int WEAPON_DEVOTION = 84;
-    int WEAPON_HAVOC = 86;
-    
-    //Shotgun
-    int WEAPON_MOZAMBIQUE = 96;
-    int WEAPON_EVA8 = 87;
-    int WEAPON_PEACEKEEPER = 103;
-    int WEAPON_MASTIFF = 95;
-    
-    //Snipers
-    int WEAPON_SENTINEL = 1;
-    int WEAPON_CHARGE_RIFLE = 83;
-    int WEAPON_LONGBOW = 85; 
-    
-    //Legendary
-    int WEAPON_WINGMAN = 109; //Emotional damage!
+    int WEAPON_FLATLINE = 89;
+    int WEAPON_HEMLOCK = 91;
     int WEAPON_PROWLER = 102;
+    int WEAPON_REPEATER = 112;
+    int WEAPON_RAMPAGE = 21;
+    int WEAPON_CAR = 113;
+
+    //Energy
+    int WEAPON_HAVOC = 86;
+    int WEAPON_DEVOTION = 84;
+    int WEAPON_LSTAR = 94;
+    int WEAPON_TRIPLETAKE = 108;
+    int WEAPON_VOLT = 111;
+    int WEAPON_NEMESIS = 114;
+
+    //Shotgun
+    int WEAPON_MOZAMBIQUE = 97;
+    int WEAPON_PEACEKEEPER = 104;
+    int WEAPON_MASTIFF = 96;
+
+    //Sniper
+    int WEAPON_LONGBOW = 85;
+    int WEAPON_CHARGE_RIFLE = 83;
+    int WEAPON_SENTINEL = 1;
+
+    //Legendary
+    int WEAPON_WINGMAN = 110;
+    int WEAPON_EVA8 = 88;
     int WEAPON_BOCEK = 2;
-    int WEAPON_KRABER = 92;
-    
-    int WEAPON_HANDS = 114;
+    int WEAPON_KRABER = 93;
+    int WEAPON_KNIFE = 166;
+
+    //Melee
+    int WEAPON_HANDS = 115;
 };
