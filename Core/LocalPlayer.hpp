73b4e1495dafa538c587a3dfddf146f191066776
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-02-14 21:55:23 +0000

    Update LocalPlayer.hpp

--- Core/LocalPlayer.hpp
+++ Core/LocalPlayer.hpp
@@ -1,121 +1,79 @@
 #pragma once
-#include "Offsets.hpp"
-#include "../Utils/Memory.hpp"
-#include "../Math/Vector2D.hpp"
-#include "../Math/Vector3D.hpp"
-#include "../Math/FloatVector2D.hpp"
-#include "../Math/FloatVector3D.hpp"
+//https://www.unknowncheats.me/forum/apex-legends/319804-apex-legends-reversal-structs-offsets.html
+ 
+//GameVersion = v3.0.57.21
+ 
+// Core    
+constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
+constexpr long OFF_LEVEL = 0x16da6b0;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x2165e48;                  //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1db73e8;                   //[Miscellaneous]->cl_entitylist
+constexpr long OFF_NAME_INDEX = 0x38;                         //nameIndex
+constexpr long OFF_NAME_LIST = 0xc5dacc0;                     //[Miscellaneous]->NameList           Not sure
+ 
+// HUD
+constexpr long OFF_VIEWRENDER = 0x73df958;                    //[Miscellaneous]->ViewRenderer
+constexpr long OFF_VIEWMATRIX = 0x11a350;                     //[Miscellaneous]->ViewMatrix
+ 
+// Buttons
+constexpr long OFF_INATTACK = 0x073e0bd8;                     //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x073e0cf0;                      //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x073e0dd8;                      //[Buttons]->in_duck
+constexpr long OFFSET_IN_FORWARD = 0x073e0b28;                //[Buttons]->in_forward
+constexpr long OFFSET_TRAVERSAL_START_TIME = 0x2ad0;          //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
+constexpr long OFFSET_TRAVERSAL_PROGRESS = 0x2acc;            //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
+constexpr long OFFSET_WALL_RUN_START_TIME = 0x355c;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
+constexpr long OFFSET_WALL_RUN_CLEAR_TIME = 0x3560;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
+constexpr long OFF_GRAPPLE_ACTIVE = 0x1e18;		      //[RecvTable.DT_NPC_Titan]       Not sure
+constexpr long OFF_GRAPPLE = 0x1d88;		              //[RecvTable.DT_NPC_Titan]        Not sure
+constexpr long OFF_GRAPPLE_ATTACHED = 0x0048;		      // [RecvTable.DT_GrappleData]       Not sure
 
-struct LocalPlayer {
-    long BasePointer;
-
-    bool IsDead;
-    bool IsInAttack;
-    bool IsKnocked;
-    bool IsZooming;
-    bool InJump;
-
-    int Team;
-    Vector3D LocalOrigin;
-    Vector3D CameraPosition;
-
-    Vector2D ViewAngles;
-    Vector2D PunchAngles;
-    Vector2D PunchAnglesPrevious;
-    Vector2D PunchAnglesDifferent;
-
-    int WeaponIndex;
-    float WeaponProjectileSpeed;
-    float WeaponProjectileScale;
-    bool IsHoldingGrenade;
-    long WeaponEntity;
-    bool weaponDiscarded;
-
-    float ZoomFOV;
-    float TargetZoomFOV;
-
-    float ViewYaw;
-    
-    FloatVector3D localOrigin;
-    FloatVector3D cameraPosition;
-    FloatVector2D viewAngles;
-    FloatVector2D punchAngles;
-    FloatVector2D punchAnglesPrev;
-    FloatVector2D punchAnglesDiff;
-    
-    int traversalProgress;
-    int traversalStartTime;
-    int worldtime;
-
-    void ResetPointer() {
-        BasePointer = 0;
-    }
-
-    void Read() {
-        BasePointer = Memory::Read<long>(OFF_REGION + OFF_LOCAL_PLAYER);
-        if (BasePointer == 0) return;
-
-        IsDead = Memory::Read<short>(BasePointer + OFF_LIFE_STATE) > 0;
-        IsKnocked = Memory::Read<short>(BasePointer + OFF_BLEEDOUT_STATE) > 0;
-        IsZooming = Memory::Read<short>(BasePointer + OFF_ZOOMING) > 0;
-        IsInAttack = Memory::Read<short>(OFF_REGION + OFF_INATTACK) > 0;
-        InJump = Memory::Read<bool>(OFF_REGION + OFF_IN_JUMP) > 0;
-
-        Team = Memory::Read<int>(BasePointer + OFF_TEAM_NUMBER);
-        localOrigin = Memory::Read<FloatVector3D>(BasePointer + OFF_LOCAL_ORIGIN);
-        LocalOrigin = Memory::Read<Vector3D>(BasePointer + OFF_LOCAL_ORIGIN);
-        CameraPosition = Memory::Read<Vector3D>(BasePointer + OFF_CAMERAORIGIN);
-        ViewAngles = Memory::Read<Vector2D>(BasePointer + OFF_VIEW_ANGLES);
-        PunchAngles = Memory::Read<Vector2D>(BasePointer + OFF_PUNCH_ANGLES);
-        PunchAnglesDifferent = PunchAnglesPrevious.Subtract(PunchAngles);
-        PunchAnglesPrevious = PunchAngles;
-
-        ViewYaw = Memory::Read<float>(BasePointer + OFF_YAW);
-        
-        cameraPosition = Memory::Read<FloatVector3D>(BasePointer + OFF_CAMERAORIGIN);
-        viewAngles = Memory::Read<FloatVector2D>(BasePointer + OFF_VIEW_ANGLES);
-        punchAngles = Memory::Read<FloatVector2D>(BasePointer + OFF_PUNCH_ANGLES);
-        punchAnglesDiff = punchAnglesPrev.subtract(punchAngles);
-        punchAnglesPrev = punchAngles;
-
-        if (!IsDead && !IsKnocked) {
-            long WeaponHandle = Memory::Read<long>(BasePointer + OFF_WEAPON_HANDLE);
-            long WeaponHandleMasked = WeaponHandle & 0xffff;
-            WeaponEntity = Memory::Read<long>(OFF_REGION + OFF_ENTITY_LIST + (WeaponHandleMasked << 5));
-
-            int OffHandWeaponID = Memory::Read<int>(BasePointer + OFF_OFFHAND_WEAPON);
-            IsHoldingGrenade = OffHandWeaponID == -251 ? true : false;
-            
-            ZoomFOV = Memory::Read<float>(WeaponEntity + OFF_CURRENTZOOMFOV);
-            TargetZoomFOV = Memory::Read<float>(WeaponEntity + OFF_TARGETZOOMFOV);
-            
-            WeaponIndex = Memory::Read<int>(WeaponEntity + OFF_WEAPON_INDEX);
-            weaponDiscarded = Memory::Read<int>(WeaponEntity + OFF_WEAPON_DISCARDED) == 1;
-            WeaponProjectileSpeed = Memory::Read<float>(WeaponEntity + OFF_PROJECTILESPEED);
-            WeaponProjectileScale = Memory::Read<float>(WeaponEntity + OFF_PROJECTILESCALE);
-            
-            
-            float traversalProgressTmp = 0.0;
-	 
-            worldtime = Memory::Read<float>(BasePointer + OFF_TIME_BASE); // Current time
-            traversalStartTime = Memory::Read<float>(BasePointer + OFFSET_TRAVERSAL_START_TIME); // Time to start wall climbing
-            traversalProgress = Memory::Read<float>(BasePointer + OFFSET_TRAVERSAL_PROGRESS); // Wall climbing, if > 0.87 it is almost over.
-        }
-    }
-
-    bool IsValid() {
-        return BasePointer != 0;
-    }
-
-    bool IsCombatReady() {
-        if (BasePointer == 0) return false;
-        if (IsDead) return false;
-        if (IsKnocked) return false;
-        return true;
-    }
-    
-    void setYaw(float angle) {
-        long ptrLong = BasePointer + OFF_VIEW_ANGLES + sizeof(float);
-        Memory::Write<float>(ptrLong, angle);
-    }
-};
+// Player
+constexpr long OFF_HEALTH = 0x0318;                           //[RecvTable.DT_Player]->m_iHealth
+constexpr long OFF_MAXHEALTH = 0x0460;                        //[RecvTable.DT_Player]->m_iMaxHealth
+constexpr long OFF_SHIELD = 0x01a0;                           //[RecvTable.DT_TitanSoul]->m_shieldHealth
+constexpr long OFF_MAXSHIELD = 0x01a4;                        //[RecvTable.DT_TitanSoul]->m_shieldHealthMax
+constexpr long OFF_CAMERAORIGIN = 0x1eb0;                     //[Miscellaneous]->CPlayer!camera_origin
+constexpr long OFF_STUDIOHDR = 0xfd0;                         //[Miscellaneous]->CBaseAnimating!m_pStudioHdr
+constexpr long OFF_BONES = 0x0d80 + 0x48;                     //m_nForceBone
+constexpr long OFF_LOCAL_ORIGIN = 0x017c;                     //[DataMap.C_BaseEntity]->m_vecAbsOrigin
+constexpr long OFF_ABSVELOCITY = 0x0170;                      //[DataMap.C_BaseEntity]->m_vecAbsVelocity
+constexpr long OFF_ZOOMING = 0x1bb1;                          //[RecvTable.DT_Player]->m_bZooming
+constexpr long OFF_TEAM_NUMBER = 0x0328;                      //[RecvTable.DT_BaseEntity]->m_iTeamNum
+constexpr long OFF_NAME = 0x0471;                             //[RecvTable.DT_BaseEntity]->m_iName
+constexpr long OFF_LIFE_STATE = 0x0680;                       //[RecvTable.DT_Player]->m_lifeState
+constexpr long OFF_BLEEDOUT_STATE = 0x26c0;                   //[RecvTable.DT_Player]->m_bleedoutState  
+constexpr long OFF_LAST_VISIBLE_TIME = 0x196d + 0x3;          //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
+constexpr long OFF_LAST_AIMEDAT_TIME = 0x196d + 0x3 + 0x8;    //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
+constexpr long OFF_VIEW_ANGLES = 0x2514 - 0x14;               //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
+constexpr long OFF_PUNCH_ANGLES = 0x2418;                     //[DataMap.C_Player]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+constexpr long OFF_YAW = 0x221c - 0x8;                        //m_currentFramePlayer.m_ammoPoolCount - 0x8
+constexpr long LOCAL_ORIGIN = 0x0188;                         // [DataMap.CBaseViewModel] -> m_localOrigin
+constexpr long OFF_MODELNAME = 0x0030;                        //m_ModelName
+constexpr long OFF_THIRDPERSON_OVERRIDE = 0x01d46910;         //thirdperson_override
+constexpr long OFF_THIRDPERSON_SHOULDERVIEW = 0x3690;         //m_thirdPersonShoulderView
+constexpr long OFF_THIRDPERSON = 0x36f4;                      //m_thirdPerson
+constexpr long OFF_THIRDPERSON_ENT = 0x368c;                  //m_hThirdPersonEnt
+constexpr long OFF_OBSERVER_MODE = 0x347c;                    //m_iObserverMode
+ 
+// Weapon 
+constexpr long OFF_WEAPON_HANDLE = 0x1914;                    //[RecvTable.DT_Player]->m_latestPrimaryWeapons
+constexpr long OFF_WEAPON_INDEX = 0x1758;                     //[RecvTable.DT_WeaponX]->m_weaponNameIndex
+constexpr long OFF_PROJECTILESCALE = 0x04ec + 0x1998;         //projectile_gravity_scale + [WeaponSettingsMeta]base
+constexpr long OFF_PROJECTILESPEED = 0x04e4 + 0x1998;         //projectile_launch_speed + [WeaponSettingsMeta]base
+constexpr long OFF_OFFHAND_WEAPON = 0x1924;                   //m_latestNonOffhandWeapons
+constexpr long OFF_CURRENTZOOMFOV = 0x15b0 + 0x00b8;          //m_playerData + m_curZoomFOV
+constexpr long OFF_TARGETZOOMFOV = 0x15b0 + 0x00bc;           //m_playerData + m_targetZoomFOV
+constexpr long OFF_SKIN = 0x0d38;                             //m_nSkin
+constexpr long OFF_TIME_BASE = 0x2068;                        //m_currentFramePlayer.timeBase
+constexpr long OFF_WEAPON_DISCARDED = 0x1579;                 //[RecvTable.DT_WeaponX]->m_discarded
+constexpr long OFF_VIEWMODELS = 0x2cf8;                       //m_hViewModels
+constexpr long OFF_CURFRAME_MODELINDEX = 0x00d8;              //[DataMap.CBaseViewModel]m_currentFrame.modelIndex
+ 
+//glow
+constexpr long OFF_HIGHLIGHT_TYPE_SIZE = 0x34;                //                                  ??
+constexpr long OFF_GLOW_ENABLE = 0x28c;                       //[DT_HighlightSettings].?            ??
+constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;                 //[DT_HighlightSettings].?            ??
+constexpr long OFF_GLOW_FIX = 0x268;                        //                                        ??
+constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C;                 //[DT_HighlightSettings].m_highlightServerActiveStates       ??
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xB943CB0;                //                                      
