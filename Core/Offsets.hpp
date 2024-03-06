2859c458aae6be2ddc34fbf62058f4b5f529d479
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-03-06 19:20:20 +0000

    Update Offsets.hpp

--- Core/Offsets.hpp
+++ Core/Offsets.hpp
@@ -1,25 +1,25 @@
 #pragma once
 //https://www.unknowncheats.me/forum/apex-legends/319804-apex-legends-reversal-structs-offsets.html
  
-//GameVersion = v3.0.57.21
+//GameVersion = v3.0.59.33
  
 // Core    
 constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
-constexpr long OFF_LEVEL = 0x16da6b0;                         //[Miscellaneous]->LevelName
-constexpr long OFF_LOCAL_PLAYER = 0x2165e48;                  //[Miscellaneous]->LocalPlayer
-constexpr long OFF_ENTITY_LIST = 0x1db73e8;                   //[Miscellaneous]->cl_entitylist
+constexpr long OFF_LEVEL = 0x16db770;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x2166fa8;                  //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1db8548;                   //[Miscellaneous]->cl_entitylist
 constexpr long OFF_NAME_INDEX = 0x38;                         //nameIndex
-constexpr long OFF_NAME_LIST = 0xc5dacc0;                     //[Miscellaneous]->NameList           Not sure
+constexpr long OFF_NAME_LIST = 0xC5E1150;                     //[Miscellaneous]->NameList           Not sure
  
 // HUD
-constexpr long OFF_VIEWRENDER = 0x73df958;                    //[Miscellaneous]->ViewRenderer
+constexpr long OFF_VIEWRENDER = 0x73e0ad8;                    //[Miscellaneous]->ViewRenderer
 constexpr long OFF_VIEWMATRIX = 0x11a350;                     //[Miscellaneous]->ViewMatrix
  
 // Buttons
-constexpr long OFF_INATTACK = 0x073e0bd8;                     //[Buttons]->in_attack
-constexpr long OFF_IN_JUMP = 0x073e0cf0;                      //[Buttons]->in_jump
-constexpr long OFF_IN_DUCK = 0x073e0dd8;                      //[Buttons]->in_duck
-constexpr long OFFSET_IN_FORWARD = 0x073e0b28;                //[Buttons]->in_forward
+constexpr long OFF_INATTACK = 0x073e1d58;                     //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x073e1e70;                      //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x073e1f58;                      //[Buttons]->in_duck
+constexpr long OFFSET_IN_FORWARD = 0x073e1ca8;                //[Buttons]->in_forward
 constexpr long OFFSET_TRAVERSAL_START_TIME = 0x2ad0;          //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
 constexpr long OFFSET_TRAVERSAL_PROGRESS = 0x2acc;            //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
 constexpr long OFFSET_WALL_RUN_START_TIME = 0x355c;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
@@ -48,9 +48,9 @@ constexpr long OFF_LAST_AIMEDAT_TIME = 0x196d + 0x3 + 0x8;    //[RecvTable.DT_Ba
 constexpr long OFF_VIEW_ANGLES = 0x2514 - 0x14;               //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
 constexpr long OFF_PUNCH_ANGLES = 0x2418;                     //[DataMap.C_Player]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
 constexpr long OFF_YAW = 0x221c - 0x8;                        //m_currentFramePlayer.m_ammoPoolCount - 0x8
-constexpr long LOCAL_ORIGIN = 0x0188;                         // [DataMap.CBaseViewModel] -> m_localOrigin
+constexpr long LOCAL_ORIGIN = 0x0188;                         // [DataMap.CBaseViewModel] ->m_localOrigin
 constexpr long OFF_MODELNAME = 0x0030;                        //m_ModelName
-constexpr long OFF_THIRDPERSON_OVERRIDE = 0x01d46910;         //thirdperson_override
+constexpr long OFF_THIRDPERSON_OVERRIDE = 0x01d47a70;         //thirdperson_override
 constexpr long OFF_THIRDPERSON_SHOULDERVIEW = 0x3690;         //m_thirdPersonShoulderView
 constexpr long OFF_THIRDPERSON = 0x36f4;                      //m_thirdPerson
 constexpr long OFF_THIRDPERSON_ENT = 0x368c;                  //m_hThirdPersonEnt
@@ -76,4 +76,4 @@ constexpr long OFF_GLOW_ENABLE = 0x28c;                       //[DT_HighlightSet
 constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;                 //[DT_HighlightSettings].?            ??
 constexpr long OFF_GLOW_FIX = 0x268;                        //                                        ??
 constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C;                 //[DT_HighlightSettings].m_highlightServerActiveStates       ??
-constexpr long OFF_GLOW_HIGHLIGHTS = 0xB943CB0;                //                                      
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xB944E30;                //highlightsettings                                      
