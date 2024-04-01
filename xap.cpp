6cb8b9b4fc320adec158aa417423ce1792b0cfa6
Gerosity <110794161+Gerosity@users.noreply.github.com>
2024-04-01 20:07:36 +0100

    Updated to game version v3.0.62.30

--- xap.cpp
+++ xap.cpp
@@ -658,7 +658,7 @@ int main(int argc, char *argv[])
     std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "By Gerosity" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "For Game Version v3.0.62.29" << std::endl;
+    std::cout << "For Game Version v3.0.62.30" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     // Initialize Overlay Window //
