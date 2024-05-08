e2d500287693ff1e830f9936eb8801b121d7a872
y0shih <101542807+y0shih@users.noreply.github.com>
2024-05-08 13:36:13 +0700

    Update Offsets.hpp

--- Core/Offsets.hpp
+++ Core/Offsets.hpp
@@ -1,75 +1,92 @@
-#pragma once
-//https://www.unknowncheats.me/forum/apex-legends/319804-apex-legends-reversal-structs-offsets.html
- 
-//GameVersion = v3.0.65.42
-
-// Core    
-constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
-constexpr long OFF_LEVEL = 0x16f94f0;                         //[Miscellaneous]->LevelName
-constexpr long OFF_LOCAL_PLAYER = 0x21830f8;                  //[Miscellaneous]->LocalPlayer
-constexpr long OFF_ENTITY_LIST = 0x1DD45F8;                   //[Miscellaneous]->cl_entitylist
-constexpr long OFF_NAME_INDEX = 0x38;                         //nameIndex
-constexpr long OFF_NAME_LIST = 0xC5FF080;                     //[Miscellaneous]->NameList
-constexpr long OFF_GAMEMODE = 0x021ba960;                     //mp_gamemode
-constexpr long OFF_SQUAD_ID = 0x0334;                          //m_squadID
- 
-// HUD
-constexpr long OFF_VIEWRENDER = 0x73f1978;                    //[Miscellaneous]->ViewRenderer
-constexpr long OFF_VIEWMATRIX = 0x11a350;                     //[Miscellaneous]->ViewMatrix
- 
-// Buttons
-constexpr long OFF_INATTACK = 0x073f2c18;                     //[Buttons]->in_attack
-constexpr long OFF_IN_JUMP = 0x073f2cf8;                      //[Buttons]->in_jump
-constexpr long OFF_IN_DUCK = 0x073f2df8;                      //[Buttons]->in_duck
-constexpr long OFF_IN_FORWARD = 0x073f2b48;                //[Buttons]->in_forward
-constexpr long OFF_TRAVERSAL_START_TIME = 0x2af0;          //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
-constexpr long OFF_TRAVERSAL_PROGRESS = 0x2aec;            //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
-constexpr long OFF_WALL_RUN_START_TIME = 0x3584;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
-constexpr long OFF_WALL_RUN_CLEAR_TIME = 0x3588;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
-constexpr long OFF_GRAPPLE_ACTIVE = 0x1e38;              //[RecvTable.DT_NPC_Titan]       Wrong (Unused)
-constexpr long OFF_GRAPPLE = 0x1da8;                      //[RecvTable.DT_NPC_Titan]        Wrong (Unused)
-constexpr long OFF_GRAPPLE_ATTACHED = 0x0048;              // [RecvTable.DT_GrappleData]       Wrong (Unused)
- 
-// Player
-constexpr long OFF_HEALTH = 0x0318;                           //[RecvTable.DT_Player]->m_iHealth
-constexpr long OFF_MAXHEALTH = 0x0460;                        //[RecvTable.DT_Player]->m_iMaxHealth
-constexpr long OFF_SHIELD = 0x01a0;                           //[RecvTable.DT_TitanSoul]->m_shieldHealth
-constexpr long OFF_MAXSHIELD = 0x01a4;                        //[RecvTable.DT_TitanSoul]->m_shieldHealthMax
-constexpr long OFF_CAMERAORIGIN = 0x1ed0;                     //[Miscellaneous]->CPlayer!camera_origin
-constexpr long OFF_STUDIOHDR = 0xff0;                         //[Miscellaneous]->CBaseAnimating!m_pStudioHdr
-constexpr long OFF_BONES = 0x0da0 + 0x48;                     //m_nForceBone
-constexpr long OFF_LOCAL_ORIGIN = 0x017c;                     //[DataMap.C_BaseEntity]->m_vecAbsOrigin
-constexpr long OFF_ABSVELOCITY = 0x0170;                      //[DataMap.C_BaseEntity]->m_vecAbsVelocity
-constexpr long OFF_ZOOMING = 0x1bd1;                          //[RecvTable.DT_Player]->m_bZooming
-constexpr long OFF_TEAM_NUMBER = 0x0328;                      //[RecvTable.DT_BaseEntity]->m_iTeamNum
-constexpr long OFF_NAME = 0x0471;                             //[RecvTable.DT_BaseEntity]->m_iName
-constexpr long OFF_LIFE_STATE = 0x0680;                       //[RecvTable.DT_Player]->m_lifeState
-constexpr long OFF_BLEEDOUT_STATE = 0x26e0;                   //[RecvTable.DT_Player]->m_bleedoutState
-constexpr long OFF_LAST_VISIBLE_TIME = 0x198d + 0x3;          //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
-constexpr long OFF_LAST_AIMEDAT_TIME = 0x198d + 0x3 + 0x8;    //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
-constexpr long OFF_VIEW_ANGLES = 0x2534 - 0x14;               //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
-constexpr long OFF_PUNCH_ANGLES = 0x2438;                     //[DataMap.C_Player]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-constexpr long OFF_YAW = 0x223c - 0x8;                        //m_currentFramePlayer.m_ammoPoolCount - 0x8
-constexpr long OFF_MODELNAME = 0x0030;                        //m_ModelName
- 
-// Weapon 
-constexpr long OFF_WEAPON_HANDLE = 0x1934;                    //[RecvTable.DT_Player]->m_latestPrimaryWeapons
-constexpr long OFF_WEAPON_INDEX = 0x1778;                     //[RecvTable.DT_WeaponX]->m_weaponNameIndex
-constexpr long OFF_PROJECTILESCALE = 0x04ec + 0x19c0;         //projectile_gravity_scale + [WeaponSettingsMeta]base
-constexpr long OFF_PROJECTILESPEED = 0x04e4 + 0x19c0;         //projectile_launch_speed + [WeaponSettingsMeta]base
-constexpr long OFF_OFFHAND_WEAPON = 0x1944;                   //m_latestNonOffhandWeapons
-constexpr long OFF_CURRENTZOOMFOV = 0x15d0 + 0x00b8;          //m_playerData + m_curZoomFOV
-constexpr long OFF_TARGETZOOMFOV = 0x15d0 + 0x00bc;           //m_playerData + m_targetZoomFOV
-constexpr long OFF_MOUSE_SENSITIVITY = 0x0216fc30;            //mouse_sensitivity (Unused)
-constexpr long OFF_SKIN = 0x0d58;                             //m_nSkin
-constexpr long OFF_TIME_BASE = 0x2088;                        //m_currentFramePlayer.timeBase
-constexpr long OFF_WEAPON_DISCARDED = 0x1599;                 //[RecvTable.DT_WeaponX]->m_discarded
-constexpr long OFF_VIEWMODELS = 0x2d18;                       //m_hViewModels
- 
-// Glow
-constexpr long OFF_HIGHLIGHT_TYPE_SIZE = 0x34;                //                         
-constexpr long OFF_GLOW_ENABLE = 0x26c;                       //Script_Highlight_GetCurrentContext
-constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;                 //Script_Highlight_SetVisibilityType     
-constexpr long OFF_GLOW_FIX = 0x268;                        //                                      
-constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C;                 //[DT_HighlightSettings].m_highlightServerActiveStates     
-constexpr long OFF_GLOW_HIGHLIGHTS = 0xBB247D0;                //highlightsettings
+    #pragma once
+    // Date 2024/05/08
+    // GameVersion = v3.0.67.34
+    //
+    //  Core
+    #include <cstdint>
+    constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
+     
+    constexpr uint64_t OFF_LEVEL = 0x17c5120; //[Miscellaneous].LevelName updated 2024/05/03
+    constexpr uint64_t OFF_LOCAL_PLAYER = 0x2258688; //[Miscellaneous].LocalPlayer updated 2024/05/03
+    constexpr uint64_t OFF_ENTITY_LIST = 0x01ea9a40 + 0xA8; //[ConVars].gamepad_aim_assist_melee updated 2024/05/03 # [Miscellaneous].cl_entitylist
+     
+    constexpr uint64_t OFF_NAME_LIST = 0x0C78F030; //[Miscellaneous].NameList updated 2024/04/09
+    constexpr uint64_t OFF_NAME_INDEX = 0x057c; //[RecvTable.DT_BaseEntity].m_instanceNameIndex
+     
+    constexpr uint64_t OFF_GAMEMODE = 0x0228ff90; //mp_gamemode
+    constexpr uint64_t OFF_SQUAD_ID = 0x0334; //m_squadID
+     
+     
+    // HUD
+    constexpr uint64_t OFF_VIEWRENDER = 0x74DADA8; //[Miscellaneous].ViewRender updated 2024/05/03
+    constexpr uint64_t OFF_VIEWMATRIX = 0x11a350; //[Miscellaneous].ViewMatrix updated 2024/05/03
+     
+    // Player
+    constexpr long OFF_MODELNAME = 0x0030;                        //m_ModelName
+    constexpr uint64_t OFF_HEALTH = 0x0318; //[RecvTable.DT_Player].m_iHealth updated 2024/05/03
+    constexpr uint64_t OFF_MAXHEALTH = 0x0460; //[RecvTable.DT_Player].m_iMaxHealth updated 2024/05/03
+    constexpr uint64_t OFF_SHIELD = 0x01a0; //[RecvTable.DT_BaseEntity].m_shieldHealth updated 2024/05/03
+    constexpr uint64_t OFF_MAXSHIELD = 0x01a4; //[RecvTable.DT_BaseEntity].m_shieldHealthMax updated 2024/05/03
+    constexpr uint64_t OFF_CAMERAORIGIN = 0x1ed0; //[Miscellaneous].CPlayer!camera_origin updated 2024/05/03
+    constexpr uint64_t OFF_STUDIOHDR = 0xff0; //[Miscellaneous].CBaseAnimating!m_pStudioHdr updated 2024/05/03
+    constexpr uint64_t OFF_BONES = 0x0da0 + 0x48; //[RecvTable.DT_BaseAnimating].m_nForceBone + 0x48 updated 2024/05/03
+    constexpr uint64_t OFF_LOCAL_ORIGIN = 0x017c; //[DataMap.C_BaseEntity].m_vecAbsOrigin updated 2024/05/03
+    constexpr uint64_t OFF_ABSVELOCITY = 0x0170; //[DataMap.C_BaseEntity].m_vecAbsVelocity updated 2024/05/03
+    constexpr uint64_t OFF_ZOOMING = 0x1bd1; //[RecvTable.DT_Player].m_bZooming updated 2024/05/03
+    constexpr uint64_t OFF_TEAM_NUMBER = 0x0328; //[RecvTable.DT_BaseEntity].m_iTeamNum updated 2024/05/03
+    constexpr uint64_t OFF_NAME = 0x0471; //[RecvTable.DT_BaseEntity].m_iName updated 2024/05/03
+    constexpr uint64_t OFF_LIFE_STATE = 0x0680; //[RecvTable.DT_Player].m_lifeState updated 2024/05/03
+    constexpr uint64_t OFF_BLEEDOUT_STATE = 0x26e0; //[RecvTable.DT_Player].m_bleedoutState updated 2024/05/03
+    constexpr uint64_t OFF_LAST_VISIBLE_TIME = 0x1990; //[Miscellaneous].CPlayer!lastVisibleTime updated 2024/05/03
+    constexpr uint64_t OFF_LAST_AIMEDAT_TIME = 0x1998; //[Miscellaneous].CWeaponX!lastCrosshairTargetTime updated 2024/05/03
+    constexpr uint64_t OFF_VIEW_ANGLES = 0x2534 - 0x14; //[RecvTable.DT_Player].m_ammoPoolCapacity - 0x14 updated 2024/05/03
+    constexpr uint64_t OFF_PUNCH_ANGLES = 0x2438; //[DataMap.C_Player].m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle updated 2024/05/03
+    constexpr uint64_t OFF_YAW = 0x223c - 0x8; //[DataMap.C_Player].m_currentFramePlayer.m_ammoPoolCount - 0x8 updated 2024/05/03
+     
+    // Weapon
+    constexpr uint64_t OFF_WEAPON_HANDLE = 0x1934; //[RecvTable.DT_BaseCombatCharacter].m_latestPrimaryWeapons updated 2024/05/03
+    constexpr uint64_t OFF_WEAPON_INDEX = 0x1778; //[RecvTable.DT_WeaponX].m_weaponNameIndex updated 2024/05/03
+    constexpr uint64_t OFF_PROJECTILESCALE = 0x19c0 + 0x04ec; //[WeaponSettingsMeta].base + [WeaponSettings].projectile_gravity_scale updated 2024/05/03
+    constexpr uint64_t OFF_PROJECTILESPEED = 0x19c0 + 0x04e4; //[WeaponSettingsMeta].base + [WeaponSettings].projectile_launch_speed updated 2024/05/03
+    constexpr uint64_t OFF_OFFHAND_WEAPON = 0x1944; //[DataMap.C_BaseCombatCharacter].m_latestNonOffhandWeapons updated 2024/05/03
+    constexpr uint64_t OFF_CURRENTZOOMFOV = 0x15d0 + 0x00b8; //[RecvTable.DT_WeaponX].m_playerData + m_curZoomFOV updated 2024/05/03
+    constexpr uint64_t OFF_TARGETZOOMFOV = 0x15d0 + 0x00bc; //[RecvTable.DT_WeaponX].m_playerData + [RecvTable.DT_WeaponPlayerData].m_targetZoomFOV updated 2024/05/03
+    constexpr uint64_t OFF_WEAPON_AMMO = 0x1554; //[RecvTable.DT_PropSurvival].m_ammoInClip updated 2024/05/03
+    constexpr uint64_t OFF_RELOADING = 0x159a; //[RecvTable.DT_WeaponX_LocalWeaponData].m_bInReload updated 2024/05/03
+    constexpr long OFF_WEAPON_DISCARDED = 0x1599;                 //[RecvTable.DT_WeaponX]->m_discarded
+    // Glow
+    constexpr uint64_t OFF_GLOW_HIGHLIGHTS = 0xADE39C0; //HighlightSettings updated 2024/04/09
+    constexpr uint64_t OFF_GLOW_ENABLE = 0x26C; //updated OFFSET_GLOW_CONTEXT_ID 7 = enabled, 2 = disabled
+    constexpr uint64_t OFF_GLOW_THROUGH_WALL = 0x26C; //Script_Highlight_SetVisibilityType updated 2 = enabled, 5 = disabled
+    constexpr uint64_t OFF_GLOW_FIX = 0x268; //updated 2024/04/12
+    constexpr uint64_t OFF_GLOW_HIGHLIGHT_ID = 0x28C; //[RecvTable.DT_HighlightSettings].m_highlightGenericContexts updated 2024/05/03 [DT_HighlightSettings].m_highlightServerActiveStates
+    constexpr uint64_t OFF_GLOW_HIGHLIGHT_TYPE_SIZE = 0x34; //updated 2024/04/12
+     
+    // Misc
+    constexpr uint64_t OFF_TIME_BASE = 0x2088; //[DataMap.C_Player].m_currentFramePlayer.timeBase updated 2024/05/03
+    constexpr uint64_t OFFSET_TRAVERSAL_START_TIME = 0x2af0; //[DataMap.C_Player].m_traversalStartTime updated 2024/05/03
+    constexpr uint64_t OFFSET_TRAVERSAL_PROGRESS = 0x2aec; //[DataMap.C_Player].m_traversalProgress updated 2024/05/03
+    constexpr uint64_t OFFSET_UID = 0x2548; //m_platformUserId=0x2578
+    constexpr uint64_t OFFSET_TIME_BASE = 0x2088; //m_currentFramePlayer.timeBase
+    constexpr uint64_t OFFSET_VIEWMODEL = 0x2d18; //m_hViewModels
+    constexpr uint64_t OFFSET_InputSystem = 0x1777f00; //InputSystem
+     
+    //
+    constexpr uint64_t OFF_INATTACK = 0x074dc020; //[Buttons].in_attack updated 2024/05/03
+    constexpr uint64_t OFF_IN_ZOOM = 0x074dc1b0; //[Buttons].in_zoom updated 2024/05/03
+    constexpr uint64_t OFF_IN_TOGGLE_DUCK = 0x074dc060; //[Buttons].in_toggle_duck updated 2024/05/03
+    constexpr uint64_t OFF_IN_FORWARD = 0x074dbf70; //[Buttons].in_forward updated 2024/05/03
+    constexpr uint64_t OFF_IN_JUMP = 0x074dc138; //[Buttons].in_jump updated 2024/05/03
+    constexpr uint64_t OFF_IN_DUCK = 0x074dc220; //[Buttons].in_duck updated 2024/05/03
+    constexpr uint64_t OFF_IN_BACKWARD = 0x074dbf98; //[Buttons].in_backward updated 2024/05/03
+    constexpr uint64_t OFF_IN_USE = 0x074dc1a0; //[Buttons].in_use updated 2024/05/03
+     
+    constexpr uint64_t OFF_IN_DUCKSTATE = 0x29e0; //[DataMap.C_Player].m_duckState updated 2024/05/03
+    constexpr uint64_t OFF_WALLRUNSTART = 0x3594; //[DataMap.C_Player].m_wallRunStartTime updated 2024/05/03
+    constexpr uint64_t OFF_WALLRUNCLEAR = 0x3598; //[DataMap.C_Player].m_wallRunClearTime float updated 2024/05/03
+    constexpr uint64_t OFF_FLAGS = 0x00c8; //[DataMap.C_Player].m_fFlags updated 2024/05/03
+    constexpr uint64_t OFF_SKYDRIVESTATE = 0x469c; //[RecvTable.DT_Player].m_skydiveState updated 2024/05/03
+     
+    constexpr uint64_t OFF_GRAPPLE_ACTIVE = 0x2cd0; //[RecvTable.DT_Player].m_grappleActive updated 2024/05/03
+    constexpr uint64_t OFF_GRAPPLE = 0x2c40; //[RecvTable.DT_Player].m_grapple updated 2024/05/03
+    constexpr uint64_t OFF_GRAPPLE_ATTACHED = 0x0048; //[RecvTable.DT_GrappleData].m_grappleAttached updated 2024/05/03
